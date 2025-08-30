ab49d4d1b6585e4e7f1aad7b0b309b17c23b4c9b
Adrian030304 <adrianromanenco1@gmail.com>
2025-08-31 02:32:31 +0300

    http code

--- main.py
+++ main.py
@@ -23,18 +23,18 @@ def create_app():
     def expired_token_callback(jwt_header, jwt_data):
         return jsonify(
             {"message":"Token has expired!", "error":"token_expired"}
-        )
+        ), 401
     
     @jwt.invalid_token_loader
     def invalid_token_callback(error):
         return jsonify(
             {"message":"Signature verification failed!", "error":"invalid_token"}
-        )
+        ), 401
     
     @jwt.unauthorized_loader
     def missing_token_callback(error):
         return jsonify(
             {"message":"Request does not contain valid token!", "error":"authorization_header"}
-        )
+        ), 401
 
     return app
