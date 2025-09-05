e61072efef14be0efecd4545289c63bce179ffc0
Adrian030304 <adrianromanenco1@gmail.com>
2025-09-05 21:20:53 +0300

    Adding flag for admin users with help of claims

--- users.py
+++ users.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt
 from models import User
 from schemas import UserSchema
 
@@ -12,15 +12,22 @@ user_bp = Blueprint(
 @user_bp.get('/all')
 @jwt_required()
 def get_all_users():
+
     page = request.args.get('page', default=1, type=int)
+    
     per_page = request.args.get('per_page', default=3, type=int)
 
-    users = User.query.paginate(
-        page=page,
-        per_page=per_page
-    )
+    claims = get_jwt()
+    if claims.get('is_staff') == True:
 
-    users_list = UserSchema().dump(users, many=True)
-    return jsonify({
-        "users": users_list
-    }), 200
+        users = User.query.paginate(
+            page=page,
+            per_page=per_page
+        )
+
+        users_list = UserSchema().dump(users, many=True)
+        return jsonify({
+            "users": users_list
+        }), 200
+    return jsonify({'message':'You are not authorized to access this.'}) , 401
+
